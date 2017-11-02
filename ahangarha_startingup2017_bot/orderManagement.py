c143560fb1fc07da46ddfd7b747fc5e0abd7bc5b
Mostafa Ahangarha <ahangarha@gmail.com>
2017-11-02 22:16:05 +0330

    Redis based user and order registration
    Basic regestration for user and orders is implimented based on Redis.
    Few enhancements are also made for logging user activities and bot
    keyboard appearance.
    
    This is just the beginning of a long journey... :)
    
    In addition, two files (.env.example and requirements) are also added.

---
+++ ahangarha_startingup2017_bot/orderManagement.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Author: Mostafa Ahangarha
+# Licence: GPLv3
+# Description: This is homework for StartingUp2017 course
+# This file consists of methods dealing with orders
+
+import redis
+r = redis.StrictRedis() # Connect with default setting (localhost:3679)
+
+def registerOrder(user_id, product_id):
+    # TODO: 
+    # Validate user_id and product_id
+    
+    # Now let's assume they are provided properly
+    
+#    r.incr('lastOrderId') # making unique order id
+#    print(r.get('lastOrderId'))
+    
+    key = 'o:{}'.format(user_id)
+    
+    if r.exists(key):
+        r.append(key, ',{}'.format(product_id))
+    else:
+        r.set(key, product_id)
