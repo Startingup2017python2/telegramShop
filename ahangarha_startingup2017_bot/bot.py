749aac14a06193c74f394ed8434e5c7072859a47
Mostafa Ahangarha <ahangarha@gmail.com>
2017-11-09 17:33:30 +0330

    FIX based on PEP8 and Travis
    FIX #12
    
    In this fix, I have altered my codes based on PEP8 standards including:
    
     - line length
     - white spaces in blank Lines
     - white spaces around operators
     - white space after commas
     - adding one blank line at the end of python files

--- ahangarha_startingup2017_bot/bot.py
+++ ahangarha_startingup2017_bot/bot.py
@@ -4,23 +4,34 @@
 # Licence: GPLv3
 # Description: This is homework for StartingUp2017 course
 
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
-from telegram import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram.ext import Updater, Filters
+from telegram.ext import CommandHandler, MessageHandler, CallbackQueryHandler
+from telegram import ReplyKeyboardMarkup, KeyboardButton
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 
 import logging
 
 import userManagement as users
 import orderManagement as orders
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', 
-                    level=logging.INFO)
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO,
+)
 
-main_keyboard = ReplyKeyboardMarkup([
-            [KeyboardButton(text='Gallery'),KeyboardButton(text='About')],
-            ], resize_keyboard = True)
+main_keyboard = ReplyKeyboardMarkup(
+    [
+        [
+            KeyboardButton(text='Gallery'),
+            KeyboardButton(text='About'),
+        ],
+    ],
+    resize_keyboard=True
+)
+
 
 # Reading token from external file
-def getToken(): 
+def getToken():
     import os
     from os.path import join, dirname
     from dotenv import load_dotenv
@@ -33,126 +44,168 @@ def getToken():
 
 def start(bot, update):
     global main_keyboard
-    
+
     # register user id
-    users.registerUser(update.message.chat_id) # Chat_id is the user's unique id
-    
+    # Chat_id is the user's unique id
+    users.registerUser(update.message.chat_id)
+
     text = """
-*Welcome to the Photo Store Bot.*  
+*Welcome to the Photo Store Bot.*
+
 You can see the *gallery* or know more *about* this bot.
 """
-    bot.send_message(chat_id=update.message.chat_id, 
-                     text=text, 
-                     parse_mode='Markdown',
-                     reply_markup = main_keyboard)
+    bot.send_message(
+        chat_id=update.message.chat_id,
+        text=text,
+        parse_mode='Markdown',
+        reply_markup=main_keyboard,
+    )
 
-def about(bot,update):
+
+def about(bot, update):
     text = """
 *About Photo Store Bot*
 
-The aim of this bot is to proide a simple solution for selling photos withing telegram.
+The aim of this bot is to proide a simple solution for selling photos withing
+ telegram.
 
 You can see (by now) sample photos to be sold in *gallery*.
 
-This bot is (being) made by Mostafa Ahangarha as the project for [StartingUp2017](https://github.com/Startingup2017python2/telegramShop) course.
-    """
+This bot is (being) made by Mostafa Ahangarha as the project for
+ [StartingUp2017](https://github.com/Startingup2017python2/telegramShop)
+ course."""
 
-    bot.send_message(chat_id=update.message.chat_id, 
-                     text=text, 
-                     parse_mode='Markdown')
-    
-def gallery(bot,update):
+    bot.send_message(
+        chat_id=update.message.chat_id,
+        text=text,
+        parse_mode='Markdown'
+    )
+
+
+def gallery(bot, update):
     import db
     galleryItams = db.getGalleryItems()
-    
+
     for i in galleryItams:
         photo_id = i['id']
         title = i['title']
         url = i['url']
-        filename = 'photos/'+ i['filename']
-        
+        filename = 'photos/' + i['filename']
+
         if url is None:
-            #If there is no URL, upload the photo
+            # If there is no URL, upload the photo
             logging.info('Uploading {}'.format(filename))
             with open(filename, "rb") as file:
-                photo=file
+                photo = file
         else:
             logging.info('Sending {} from url'.format(filename))
-            photo=url
-        
-        bot.send_photo(chat_id=update.message.chat_id, 
-                       photo=photo)
-        
-        keyboard=InlineKeyboardMarkup([[(InlineKeyboardButton(text='More...', callback_data="info_{}".format(photo_id)))]])
-        
-        bot.send_message(chat_id=update.message.chat_id, 
-                         text=title, 
+            photo = url
+
+        bot.send_photo(
+            chat_id=update.message.chat_id,
+            photo=photo,
+        )
+
+        keyboard = InlineKeyboardMarkup(
+            [
+                [
+                    (
+                        InlineKeyboardButton(
+                            text='More...',
+                            callback_data="info_{}".format(photo_id)
+                        )
+                    )
+                ]
+            ]
+        )
+
+        bot.send_message(chat_id=update.message.chat_id,
+                         text=title,
                          parse_mode='Markdown',
                          reply_markup=keyboard)
 
+
 def buy(bot, update):
     query = update.callback_query
-    data = query['data'] # Format: job_id
-    
-    item_id = data.split('_')[1]
-    
-    orders.registerOrder(query.message.chat_id, item_id)
-    logging.info("New order registered: User {}({}) ordered {}".format(query.message.chat_id, query.message.chat['username'], item_id))
+    data = query['data']  # Format: job_id
 
-    text = 'You order for the photo with id={} is registered successfully. But still we cannot ship it!'.format(item_id)
+    item_id = data.split('_')[1]
+
+    orders.registerOrder(query.message.chat_id, item_id)
+    logging.info(
+        "New order registered: User {}({}) ordered {}".format(
+            query.message.chat_id,
+            query.message.chat['username'],
+            item_id,
+        )
+    )
+
+    text = """You order for the photo with id={} is registered successfully.
+ But still we cannot ship it!""".format(item_id)
     bot.edit_message_text(chat_id=query.message.chat_id,
-                 text=text,
-                 parse_mode='Markdown',
-                 message_id=query.message.message_id)
+                          text=text,
+                          parse_mode='Markdown',
+                          message_id=query.message.message_id)
+
+
 def info(bot, update):
     query = update.callback_query
-    data = query['data'] # Format: job_id
-    
+    data = query['data']  # Format: job_id
+
     item_id = data.split('_')[1]
-    
+
     import db
     galleryItam = db.getGalleryItem(item_id)
-    
+
     keyboardBtn = []
-    
-    if galleryItam == None:
+
+    if galleryItam is None:
         logging.info('No item found in database with id={}'.format(item_id))
         text = "No Item found (or wrong item id was sent)"
     else:
-        photo_id    = galleryItam['id']
-        title       = galleryItam['title']
-        
+        photo_id = galleryItam['id']
+        title = galleryItam['title']
+
         # The following information is for presentation. Later, all of them
-        # must 
-        text        = """*{}*  
+        # must be fetched from db
+        text = """*{}*
 _By: Mostafa Ahangarha_
 
-*Size:* 100x70 cm  
+*Size:* 100x70 cm
 *On:* Sep 15, 2017
 
 *Price:* $ 30""".format(title)
-        keyboardBtn.append([(InlineKeyboardButton(text='Buy', callback_data="buy_{}".format(photo_id)))])
-    
-    keyboard=InlineKeyboardMarkup(keyboardBtn)    
+        keyboardBtn.append(
+            [
+                (
+                    InlineKeyboardButton(
+                        text='Buy',
+                        callback_data="buy_{}".format(photo_id),
+                    )
+                ),
+            ]
+        )
+
+    keyboard = InlineKeyboardMarkup(keyboardBtn)
 
     bot.edit_message_text(chat_id=query.message.chat_id,
-                 text=text,
-                 parse_mode='Markdown',
-                 message_id=query.message.message_id,
-                 reply_markup=keyboard)
+                          text=text,
+                          parse_mode='Markdown',
+                          message_id=query.message.message_id,
+                          reply_markup=keyboard)
 
 
-def msgRedirect(bot,update):
+def msgRedirect(bot, update):
     if update.message.text == 'About':
         about(bot, update)
     elif update.message.text == 'Gallery':
         gallery(bot, update)
 
 
-def callbackQueryManager(bot,update):
+def callbackQueryManager(bot, update):
     query = update.callback_query
-    data = query['data'] # Format: job_id
-    
+    data = query['data']  # Format: job_id
+
     job = data.split('_')[0]
     if job == 'info':
         info(bot, update)
--- ahangarha_startingup2017_bot/userManagement.py
+++ ahangarha_startingup2017_bot/userManagement.py
@@ -6,12 +6,12 @@
 # This file consists of methods dealing with users
 
 import redis
-r = redis.StrictRedis() # Connect with default setting (localhost:3679)
+r = redis.StrictRedis()  # Connect with default setting (localhost:3679)
+
 
 def registerUser(user_id=0):
-    # TODO: 
+    # TODO:
     # Validate user_id to be integer and greater than 0
     # Now let's assume it is provided properly
     # Saving user_id as (key='u:user_id', value='1')
-    
     r.set('u:{}'.format(user_id), '1')
